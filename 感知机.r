a4737d021fbaf3eeddf5ed758c153f80aa378a82
rjXuuuuu <41714347+rjXuuuuu@users.noreply.github.com>
2018-10-12 21:21:52 +0800

    Update 感知机.r

--- 感知机.r
+++ 感知机.r
@@ -21,8 +21,4 @@ percept <- function(data = data,eta = eta ){
 
 data <- matrix(c(3,3,1,4,3,1,1,1,-1),nr=3,byrow=T)
 percept(data = data,eta = 1)
---------------------- 
-作者：littlely_ll 
-来源：CSDN 
-原文：https://blog.csdn.net/littlely_ll/article/details/53674517?utm_source=copy 
-版权声明：本文为博主原创文章，转载请附上博文链接！
+
